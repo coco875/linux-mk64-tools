1367189f5ea590988de87147a19b2ce51ca07c39
coco875 <pereira.jannin@gmail.com>
2024-01-30 16:41:09 +0100

    change mingw path

--- install_mips.sh
+++ install_mips.sh
@@ -1,6 +1,7 @@
 mv mips-tools/tools tools -r
 chmod +x tools/mio0
 chmod +x tools/n64graphics
+chmod +x tools/tkmk00
 chmod +x tools/ido5.3_recomp/*
 mv mips-tools/*.so* /lib64
 mv mips-tools/* /usr/bin
