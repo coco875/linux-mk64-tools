6c55daf019b23cd5105894b2857b046b8015f8ce
coco875 <59367621+coco875@users.noreply.github.com>
2024-01-30 14:01:58 +0100

    Add files via upload

---
+++ download_mips.sh
@@ -0,0 +1,5 @@
+wget https://github.com/coco875/linux-mk64-tools/releases/download/0.0.1/mips-tools-chain.zip
+mkdir mips-tools
+unzip mips-tools-chain.zip -d ./mips-tools
+rm mips-tools-chain.zip
+chmod +x ./mips-tools/*
---
+++ install_mips.sh
@@ -0,0 +1,3 @@
+mv mips-tools/*.so* /lib/x86_64-linux-gnu
+mv mips-tools/* /usr/bin
+rm -rf mips-tools
