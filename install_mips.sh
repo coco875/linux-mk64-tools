6d12d3a5047351504839643153f1e9d9b09691ed
coco875 <pereira.jannin@gmail.com>
2024-01-30 15:15:46 +0100

    fix linux and enable windows

--- install_mips.sh
+++ install_mips.sh
@@ -5,6 +5,7 @@ chmod +x tools/ido5.3_recomp/cc
 chmod +x tools/ido5.3_recomp/cfe
 chmod +x tools/ido5.3_recomp/uopt
 chmod +x tools/ido5.3_recomp/ugen
+chmod +x tools/ido5.3_recomp/as1
 mv mips-tools/*.so* /lib64
 mv mips-tools/* /usr/bin
 rm -rf mips-tools
