41c246fb33fd7a4430725c1a9bc99794184a8de7
coco875 <59367621+coco875@users.noreply.github.com>
2024-01-30 15:05:15 +0100

    Update install_mips.sh

--- install_mips.sh
+++ install_mips.sh
@@ -4,6 +4,7 @@ chmod +x tools/n64graphics
 chmod +x tools/ido5.3_recomp/cc
 chmod +x tools/ido5.3_recomp/cfe
 chmod +x tools/ido5.3_recomp/uopt
+chmod +x tools/ido5.3_recomp/ugen
 mv mips-tools/*.so* /lib64
 mv mips-tools/* /usr/bin
 rm -rf mips-tools
